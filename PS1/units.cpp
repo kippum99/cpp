71e030926d7e9e9afa1cd009f237c980553e2914
June Kim <jooeun@caltech.edu>
2020-02-03 12:38:03 -0800

    Lab 1 redo

--- PS1/units.cpp
+++ PS1/units.cpp
@@ -1,5 +1,7 @@
 #include "units.h"
 
+// Contains implementation of functions in units.h
+
 // Two-argument constructor - stores given value and units
 UValue::UValue(double value, string units) {
     this->value = value;
@@ -19,13 +21,13 @@ string UValue::get_units() {
 // Converts units of a UValue input to to_units
 UValue convert_to(UValue input, string to_units) {
     if (input.get_units() == "lb" && to_units == "kg") {
-        return UValue(input.get_value() * 0.45, "kg");
+        return UValue{input.get_value() * 0.45, "kg"};
     }
     else if (input.get_units() == "gal" && to_units == "L") {
-        return UValue(input.get_value() * 3.79, "L");
+        return UValue{input.get_value() * 3.79, "L"};
     }
     else if (input.get_units() == "mi" && to_units == "km") {
-        return UValue(input.get_value() * 1.6, "km");
+        return UValue{input.get_value() * 1.6, "km"};
     }
     else {
         return input;
--- PS1/units.h
+++ PS1/units.h
@@ -2,16 +2,18 @@
 
 using namespace std;
 
-// A united-value class
+// A united-value class holding a double value and string units for the value
 class UValue {
     double value;   // value itself
     string units;   // units for the value
 
 public:
+    // Two-argument constructor - stores given value and units
     UValue(double value, string units);
 
-    double get_value();
-    string get_units();
+    double get_value();   // Returns value of a UValue
+    string get_units();   // Returns units of a UValue
 };
 
+// Converts units of a UValue input to to_units
 UValue convert_to(UValue input, string to_units);
